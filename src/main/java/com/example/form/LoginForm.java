b9e7c35df3af63877300a108491be9350abf1bb8
ymnk10 <kseisaimin10@gmail.com>
2024-05-21 17:51:10 +0900

    ログイン時に使用するフォームクラスの実装

---
+++ src/main/java/com/example/form/LoginForm.java
@@ -0,0 +1,38 @@
+package com.example.form;
+
+/**
+ * ログイン時に使用するフォームクラス.
+ *
+ * @author io.yamanaka
+ *
+ */
+public class LoginForm {
+    /** メールアドレス */
+    private String mailAddress;
+    /** パスワード */
+    private String password;
+
+    @Override
+    public String toString() {
+        return "LoginForm{" +
+                "mailAddress='" + mailAddress + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public String getMailAddress() {
+        return mailAddress;
+    }
+
+    public void setMailAddress(String mailAddress) {
+        this.mailAddress = mailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
