3d4a07ae37fa65b2bb9b14a0a26ea45c834c5330
ymnk10 <kseisaimin10@gmail.com>
2024-05-21 15:02:11 +0900

    モック画面の実装

--- src/main/java/com/example/service/AdministratorService.java
+++ src/main/java/com/example/service/AdministratorService.java
@@ -18,8 +18,24 @@ public class AdministratorService {
     @Autowired
     private AdministratorRepository administratorRepository = new AdministratorRepository();
 
+    /**
+     * 管理者情報を挿入するメソッド.
+     *
+     * @param administrator 管理者オブジェクト
+     */
     public void insert(Administrator administrator) {
         administratorRepository.insert(administrator);
+    }
+
+    /**
+     * ログイン処理をするメソッド.
+     *
+     * @param mailAddress メールアドレス
+     * @param password パスワード
+     * @return メールアドレスとパスワードに合致した管理者オブジェクト
+     */
+    public Administrator login(String mailAddress, String password) {
+        return administratorRepository.findByMailAddressAndPassword(mailAddress, password);
     }
 
 }
