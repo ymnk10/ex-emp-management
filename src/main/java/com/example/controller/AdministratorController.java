3356190277076c1d0582e0366a1746281ae1bd2b
ymnk10 <kseisaimin10@gmail.com>
2024-05-21 13:53:17 +0900

    管理者登録画面に入力された管理者情報を登録するメソッドの実装

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -1,11 +1,14 @@
 package com.example.controller;
 
+import com.example.domain.Administrator;
 import com.example.form.InsertAdministratorForm;
 import com.example.repository.AdministratorRepository;
 import com.example.service.AdministratorService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -21,7 +24,7 @@ public class AdministratorController {
     private AdministratorService administratorService = new AdministratorService();
 
     /**
-     * 管理者登録画面に入力された管理者情報を登録するメソッド
+     * 管理者登録画面を表示させるメソッド
      *
      * @param form 登録する管理者情報のフォーム
      * @return "administrator/insert" フォワード先
@@ -29,5 +32,20 @@ public class AdministratorController {
     @GetMapping("/toInsert")
     public String toInsert(InsertAdministratorForm form){
         return "administrator/insert";
+    }
+
+
+    /**
+     * 管理者登録画面に入力された管理者情報を登録するメソッド
+     * @param form 登録する管理者情報のフォーム
+     * @return "redirect:/" リダイレクト先
+     */
+    @PostMapping("/insert")
+    public String insert(InsertAdministratorForm form){
+        Administrator administrator = new Administrator();
+        BeanUtils.copyProperties(form, administrator);
+//        administrator.setId(form.get);
+        administratorService.insert(administrator);
+        return "redirect:/";
     }
 }
